d795a971d13944604994a1feab622ebf07545878
caoql98 <46946418+caoql98@users.noreply.github.com>
2025-11-18 13:31:01 +0800

    Add files via upload

---
+++ CogVideo-main/download.py
@@ -0,0 +1,6 @@
+import os
+from huggingface_hub import snapshot_download
+
+weight_dir = "./weights"
+os.makedirs(weight_dir, exist_ok=True)
+snapshot_download(repo_id="lixirui142/physics", local_dir=weight_dir)
